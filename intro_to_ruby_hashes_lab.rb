db303e4f02e7edc569f656bb1c9a117c62a79e9a
Liz Love <love.elizabeth1989@gmail.com>
2020-05-22 18:11:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,8 +8,16 @@ def monopoly_with_second_tier
     monopoly = {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => {},
-      :names => {}
+      :rent_in_dollars => {
+        :one_piece_owned => 25,
+        :two_piece_owned => 50,
+        :three_piece_owned => 100,
+        :four_piece_owned => 200
+      },
+      :names => {
+        :reading_railroad => {},
+        :pennsylvania
+      }
     }
   }
 
